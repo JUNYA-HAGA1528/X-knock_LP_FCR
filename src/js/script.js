ab84716bc4a70c6bb86ccc99027845480fd79fe3
hagajunya <n0812034@yahoo.co.jp>
2024-11-12 13:34:35 +0900

    2024/11/12

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,20 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
-
+        // アコーディオン
+        document.querySelectorAll('.accordion__header').forEach(button => {
+            button.addEventListener('click', () => {
+                const accordionItem = button.parentElement;
+        
+                // アコーディオンの開閉
+                accordionItem.classList.toggle('active');
+        
+                // 他のアコーディオンを閉じる
+                document.querySelectorAll('.accordion__item').forEach(item => {
+                if (item !== accordionItem) {
+                    item.classList.remove('active');
+                }
+                });
+            });
+        });
 });
